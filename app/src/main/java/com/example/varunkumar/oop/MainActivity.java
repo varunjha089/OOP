debaae4c97080204bc5aa89702ccd58d4fe1c3d6
VARUN KUMAR <varunjha089@gmail.com>
2018-04-09 19:23:43 +0530

    Initial commit

---
+++ app/src/main/java/com/example/varunkumar/oop/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.varunkumar.oop;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "VARUN KUMAR";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
+    }
+}
